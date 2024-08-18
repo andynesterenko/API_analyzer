bbbd1906dee65e1d32720e0a8554ee09cad19131
andynesterenko <andrewnesterenko2124@gmail.com>
2024-08-18 14:10:11 +0300

    init: Create Express JS basic application:
    - Installed the necessary libraries to work with the framework, env files and database
    - Created the main file and wrote the basic code to run the application

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express from "express"
+import dotenv from "dotenv"
+
+const app = express()
+app.use(express.json())
+dotenv.config()
+
+app.listen(process.env.PORT, () => {
+    console.log(`Application is listening on ${process.env.PORT} port`)
+})
