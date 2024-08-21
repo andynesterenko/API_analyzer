f5b6d1cbfc8ddff6d33406a4922c9cd35185000e
andynesterenko <andrewnesterenko2124@gmail.com>
2024-08-21 12:27:42 +0300

    feat: Created a controller to process axios request:
    - Wrote logic to get data from user API
    - Made basic validation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import express from "express"
 import dotenv from "dotenv"
+import axios from "axios"
 
 const app = express()
 app.use(express.json())
 dotenv.config()
+
+app.post("/api/apiURL", async (req, res) => {
+    
+    const {apiURL} = req.body;
+
+    if (apiURL) {
+
+        const {data} = await axios.get(apiURL)
+
+        return res.status(200).json({
+            data
+        })
+    }
+    else {
+        return res
+          .status(400)
+          .json({ message: "API URL is missing or has an invalid format" });
+    }
+    
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Application is listening on ${process.env.PORT} port`)
